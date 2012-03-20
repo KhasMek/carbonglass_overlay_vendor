ab0a41b7efe613110679bd78b242e71647238a95
KhasMek <Boushh@gmail.com>
2012-03-20 11:21:45 -0600

    initial commit with all kinds of cleanup left to do

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,23 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/carbonglass_crespo.mk \
+    $(LOCAL_DIR)/carbonglass_crespo4g.mk \
+    $(LOCAL_DIR)/carbonglass_inc.mk \
+    $(LOCAL_DIR)/carbonglass_maguro.mk \
+    $(LOCAL_DIR)/carbonglass_captivatemtd.mk \
+    $(LOCAL_DIR)/carbonglass_fascinatemtd.mk \
+    $(LOCAL_DIR)/carbonglass_galaxysmtd.mk \
+    $(LOCAL_DIR)/carbonglass_otter.mk \
+    $(LOCAL_DIR)/carbonglass_p4.mk \
+    $(LOCAL_DIR)/carbonglass_p4vzw.mk \
+    $(LOCAL_DIR)/carbonglass_p4wifi.mk \
+    $(LOCAL_DIR)/carbonglass_stingray.mk \
+    $(LOCAL_DIR)/carbonglass_supersonic.mk \
+    $(LOCAL_DIR)/carbonglass_tenderloin.mk \
+    $(LOCAL_DIR)/carbonglass_toro.mk \
+    $(LOCAL_DIR)/carbonglass_vibrantmtd.mk \
+    $(LOCAL_DIR)/carbonglass_vivow.mk \
+    $(LOCAL_DIR)/carbonglass_galaxytab7c.mk \
+    $(LOCAL_DIR)/carbonglass_wingray.mk \
+    $(LOCAL_DIR)/carbonglass_p999.mk \
+    $(LOCAL_DIR)/carbonglass_p990.mk
+
